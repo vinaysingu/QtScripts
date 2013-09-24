9af76d5b1e801ec1ec926ad79fe4358c117ef946
Shyamsunder <shyamsunder@zenqa.com>
2013-09-24 19:49:54 +0530

    Project Accounting

---
+++ suite_Project Accounting/tst_Project Acc/test.js
@@ -0,0 +1,220 @@
+function main()
+{
+    
+    //-----includes-----
+    source(findFile("scripts","functions.js"));
+    
+    //-----login Application-----
+    loginAppl("CONFIGURE"); 
+    
+    
+    
+    //--------Edit the User Preferences----
+    try
+    {
+        if(OS.name=="Darwin")
+        {
+            
+            waitForObjectItem(":xTuple ERP:*_QMenuBar_2", "Products");
+            activateItem(":xTuple ERP:*_QMenuBar_2", "Products");
+            waitForObjectItem(":xTuple ERP: OpenMFG Edition.Products_QMenu","Setup...");
+            activateItem(":xTuple ERP: OpenMFG Edition.Products_QMenu", "Setup...");
+            
+        } 
+        else
+        {
+            waitForObjectItem(":xTuple ERP:*_QMenuBar_2", "System");
+            activateItem(":xTuple ERP:*_QMenuBar_2", "System");
+            
+            waitForObjectItem(":xTuple ERP:*.System_QMenu", "Preferences...");
+            activateItem(":xTuple ERP:*.System_QMenu", "Preferences...");
+        }
+        
+        snooze(2);
+        waitForObject(":Interface Options.Show windows inside workspace_QRadioButton");
+        snooze(1);
+        if(!findObject(":Interface Options.Show windows inside workspace_QRadioButton").checked)
+            clickButton(":Interface Options.Show windows inside workspace_QRadioButton");
+        snooze(0.3);
+        if(object.exists(":Notice.Notice_QDialog"))
+        {
+            waitForObject(":Notice.Remind me about this again._QCheckBox");
+            if(findObject(":Notice.Remind me about this again._QCheckBox").checked)
+                clickButton(":Notice.Remind me about this again._QCheckBox");
+            snooze(0.1);
+            waitForObject(":Notice.OK_QPushButton");
+            clickButton(":Notice.OK_QPushButton");            
+        }
+        
+        waitForObject(":User Preferences.Save_QPushButton");
+        clickButton(":User Preferences.Save_QPushButton");
+        
+        waitForObjectItem(":xTuple ERP:*_QMenuBar_2", "System");
+        activateItem(":xTuple ERP:*_QMenuBar_2", "System");
+        waitForObjectItem(":xTuple ERP:*.System_QMenu", "Rescan Privileges");
+        activateItem(":xTuple ERP:*.System_QMenu", "Rescan Privileges");
+    }
+    catch(e)
+    {
+        test.fail("Error in editing preferences" + e);
+    }
+    
+    
+    waitForObjectItem(":xTuple ERP:*_QMenuBar_2", "System");
+    activateItem(":xTuple ERP:*_QMenuBar_2", "System");
+    waitForObjectItem(":xTuple ERP:*.System_QMenu", "Exit xTuple ERP...");
+    activateItem(":xTuple ERP:*.System_QMenu", "Exit xTuple ERP...");
+    
+    snooze(4);
+    if(OS.name=="Linux")
+        startApplication("xtuple.bin");
+    
+    else
+        startApplication("xtuple");
+    
+    snooze(2);
+    
+    loginAppl("CONFIGURE"); 
+    
+    
+    //-----Variable Declaration-----
+    
+    
+    //---find Application Edition------ 
+    try
+    {
+        waitForObjectItem(":xTuple ERP:*_QMenuBar_2", "System");
+        activateItem(":xTuple ERP:*_QMenuBar_2", "System");
+        waitForObjectItem(":xTuple ERP:*.System_QMenu", "Setup...");
+        activateItem(":xTuple ERP:*.System_QMenu", "Setup...");     
+        snooze(1);
+        if(findObject(":Setup._tree_XTreeWidget").itemsExpandable== true)
+        {
+            waitForObject(":Configure.Database_QModelIndex");
+            mouseClick(":Configure.Database_QModelIndex", 41, 6, 0, Qt.LeftButton);
+        }
+        else
+        {    
+            waitForObject(":_tree.Configure_QModelIndex");
+            mouseClick(":_tree.Configure_QModelIndex", -10, 5, 0, Qt.LeftButton);
+            waitForObject(":Configure.Database_QModelIndex");
+            mouseClick(":Configure.Database_QModelIndex", 41, 6, 0, Qt.LeftButton);
+        }
+        
+        waitForObject(":Database Information.*_QLabel");
+        var appEdition = findObject(":Database Information.*_QLabel").text;
+        
+        if(object.exists(":_stack.Use toolbars on displays when available_QCheckBox"))
+        {
+            waitForObject(":_stack.Use toolbars on displays when available_QCheckBox");
+            snooze(1);
+            if(!(findObject(":_stack.Use toolbars on displays when available_QCheckBox").checked))
+                clickButton(":_stack.Use toolbars on displays when available_QCheckBox");
+        }
+        
+        waitForObject(":Setup.Save_QPushButton");
+        clickButton(":Setup.Save_QPushButton");
+    }
+    catch(e)
+    {
+        test.fail("Error in capturing database information" + e);
+    }
+    
+    
+    //-----Setting Encryption Key-----
+    try
+    {
+        waitForObjectItem(":xTuple ERP:*_QMenuBar_2", "System");
+        activateItem(":xTuple ERP:*_QMenuBar_2", "System");
+        waitForObjectItem(":xTuple ERP:*.System_QMenu", "Setup...");
+        activateItem(":xTuple ERP:*.System_QMenu", "Setup...");
+        waitForObject(":Configure.Encryption_QModelIndex");
+        mouseClick(":Configure.Encryption_QModelIndex", 40, 8, 0, Qt.LeftButton);
+        
+        snooze(1);
+        if(object.exists(":OK_QPushButton"))
+        {	
+            clickButton(":OK_QPushButton");
+            test.fatal("Please Define the Encryption path"); 
+        }
+        waitForObject(":Setup.Save_QPushButton");
+        clickButton(":Setup.Save_QPushButton");
+    }
+    catch(e)
+    {
+        test.fail("Error in setting the encryption key" + e);
+    } 
+    
+    //------ Enabling Project Accounting -------
+    
+    try{
+        activateItem(waitForObjectItem(":xTuple ERP:*_QMenuBar_2", "CRM"));
+        activateItem(waitForObjectItem(":xTuple ERP:*CRM_QMenu", "Setup..."));
+        snooze(2);
+         if(!findObject(":_stack.Enable Project Accounting_QCheckBox").checked)
+        clickButton(waitForObject(":_stack.Enable Project Accounting_QCheckBox"));
+        
+        clickButton(waitForObject(":View Check Run.Save_QPushButton"));
+        
+        test.pass("Project Accounting Enabled successfully");
+        }
+    catch(e)
+       {
+        test.fail("Error in Enabling Project Accounting "+e);
+         if(object.exists(":View Check Run.Save_QPushButton"))
+            clickButton(":View Check Run.Save_QPushButton");
+       }
+    
+    //--------- Rescaning the Privileges -----
+    
+    try{
+         activateItem(waitForObjectItem(":xTuple ERP:*_QMenuBar_2", "System"));
+         activateItem(waitForObjectItem(":xTuple ERP:*.System_QMenu", "Rescan Privileges"));
+     }
+    catch(e)
+    {
+        test.fail("Error in Rescaning the Privileges"+e);
+    }
+    
+    
+    //-------------- Project Creation ---------
+    
+    var projNum = "PROJECT 5";
+    try{
+    activateItem(waitForObjectItem(":xTuple ERP:*_QMenuBar_2", "CRM"));
+    activateItem(waitForObjectItem(":xTuple ERP:*CRM_QMenu", "Project"));
+    activateItem(waitForObjectItem(":xTuple ERP:*.Project_QMenu", "List..."));
+    clickButton(waitForObject(":Open Sales Orders.New_QToolButton_2"));
+    snooze(1);
+    type(waitForObject(":Project._number_XLineEdit"), "PROJECT 3");
+    nativeType("<Tab>");
+    type(waitForObject(":Project._name_XLineEdit"), projNum);
+    nativeType("<Tab>");
+    type(waitForObject(":_infoGroup.VirtualClusterLineEdit_CRMAcctLineEdit"), "TTOYS");
+    nativeType("<Tab>");
+    type(waitForObject(":_filterGroup.XDateEdit_XDateEdit"), "0");
+    type(waitForObject(":_filterGroup.XDateEdit_XDateEdit_2"), "0");
+    type(waitForObject(":_scheduleGroup.XDateEdit_XDateEdit"), "0");
+    nativeType("<Tab>");
+    type(waitForObject(":groupBox_2.VirtualClusterLineEdit_ContactClusterLineEdit"), "Jake Sweet");
+    nativeType("<Tab>");
+    snooze(0.5);
+    waitForObject(":Project._status_XComboBox");
+    clickItem(":Project._status_XComboBox", "In-Process",0, 0, 5, Qt.LeftButton);
+    snooze(1);
+    clickButton(waitForObject(":View Check Run.Save_QPushButton"));
+    clickButton(waitForObject(":Vendors.Query_QToolButton_3"));
+    snooze(0.5);
+    if(object.exists("{column='0' container=':_list_XTreeWidget_13' text='"+projNum+"' type='QModelIndex'}"))
+        test.pass("Project Created successfully and found in the list");
+    else
+        test.fail("Created Project was not found in the list");
+        
+    clickButton(waitForObject(":Vendors.Close_QToolButton_3"));
+    }
+    catch(e)
+    {
+        test.fail("Error in creating Project"+e);
+    }
+    
+}
